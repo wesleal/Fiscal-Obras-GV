6b2d7c63ff81d941a8f151fbe03a62d9e65919a0
wesleal <66813055+wesleal@users.noreply.github.com>
2025-12-04 18:22:48 -0300

    Create supabase.ts

---
+++ src/lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
+  "https://xbatnbtcfmwuajplnoxt.supabase.co",
+  "sb_publishable_r5xDyrwPRf7-TFmGa4d2qw_p7DBpaHP"
+)
